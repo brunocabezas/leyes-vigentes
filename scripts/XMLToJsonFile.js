4f0fb52db5350d4be9f11a91445420ed59e0b732
bruno <bruno@osx.com>
2018-11-05 22:54:59 -0300

    added babel and jest for testing, added first test

--- scripts/XMLToJsonFile.js
+++ scripts/XMLToJsonFile.js
@@ -1,11 +1,27 @@
 const fs = require("fs");
+const path = require("path");
 const parseString = require("/usr/local/lib/node_modules/xml2js/lib/xml2js.js")
   .parseString;
 
 // Writes to existing or not file leyes.json
-const toFile = (file: "./data/leyes.xml", output: "./data/leyes.json") =>
-  parseString(fs.readFileSync(file), (err, result) => {
-    fs.writeFile(file, JSON.stringify(result), "utf8");
+const toFile = (
+  file = "../data/small_leyes.xml",
+  output = "../data/leyes3.json"
+) => {
+  const filePath = path.resolve(__dirname, file);
+  // console.log(filePath, fs.readFileSync(__dirname, file));
+  return parseString(fs.readFileSync(filePath, "utf8"), (readError, result) => {
+    // console.log(result, readError);
+    if (readError) throw readError;
+    else if (!result) throw result;
+    else if (result) {
+      const writePath = path.resolve(__dirname, output);
+      fs.writeFile(writePath, JSON.stringify(result), "utf8", writeError => {
+        if (writeError) throw writeError;
+        else console.log("No error. File written: " + writePath);
+      });
+    }
   });
+};
 
-export default toFile;
+module.exports = toFile();
